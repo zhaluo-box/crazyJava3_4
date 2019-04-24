d3afed45f11280ea1348fb03f30e6c4c837bd4de
wangmingzhe <wangmingzhe@travelsky.com>
2019-04-24 10:58:40 +0800

    代理模式学习

---
+++ src/main/java/com/zhaluobox/designMode/proxymode1/JiaShi.java
@@ -0,0 +1,14 @@
+package com.zhaluobox.designMode.proxymode1;
+
+/**
+ * I'm glad to share my knowledge with you all.
+ */
+public class JiaShi implements KindWomen {
+    public void happyWithMan() {
+        System.out.println("贾氏正在Happy中......");
+    }
+
+    public void makeEyesWithMan() {
+        System.out.println("贾氏抛媚眼");
+    }
+}
