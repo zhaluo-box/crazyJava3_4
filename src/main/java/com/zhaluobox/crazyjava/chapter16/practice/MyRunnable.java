5875bd34f8d50ec9293728e481242a332b5c5682
wangmingzhe <wangmingzhe@travelsky.com>
2019-05-08 17:14:36 +0800

    演示后台守护线程

--- src/main/java/com/zhaluobox/crazyjava/chapter16/practice/MyRunnable.java
+++ src/main/java/com/zhaluobox/crazyjava/chapter16/practice/MyRunnable.java
@@ -14,4 +14,29 @@ public class MyRunnable implements Runnable {
             System.out.println(Thread.currentThread().getName()+ " - " + i);
         }
     }
+
+    /**
+     * 演示后台守护线程
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // Exception in thread "main" java.lang.IllegalThreadStateException
+        // 设置后台线程必须在线程启动之前
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000 ; i++) {
+                System.out.println(Thread.currentThread().getName()+": " + i);
+            }
+        },"t1 后台线程");
+        t1.setDaemon(true);
+        t1.start();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName()+": " + i);
+            if(i == 10) {
+                new Thread(new MyRunnable(), "子线程").start();
+            }
+        }
+    }
 }
