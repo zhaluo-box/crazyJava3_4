cc8fdfe9a74164e7b28a5dda4553bdfbd57ac949
wangmingzhe <wangmingzhe@travelsky.com.cn>
2021-04-01 17:23:29 +0800

    容器复习

---
+++ src/main/java/com/zhaluobox/designMode/prototype/geek/SearchWord.java
@@ -0,0 +1,16 @@
+package com.zhaluobox.designMode.prototype.geek;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SearchWord {
+
+    private long lastUpdateTime;
+    private String keyword;
+    private int count;
+
+    public SearchWord(String keyword, int count, long lastUpdateTime) {
+    }
+}
